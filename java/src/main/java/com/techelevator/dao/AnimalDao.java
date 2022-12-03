d090015a79ee00671c33c8b6adf3a073ca6c8042
Derya Yazici <derbaltaci61@gmail.com>
2022-12-03 16:03:11 -0500

    interface

---
+++ java/src/main/java/com/techelevator/dao/AnimalDao.java
@@ -0,0 +1,16 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Animal;
+
+import java.util.List;
+
+public interface AnimalDao {
+
+    List<Animal> findAll();
+
+    Animal getAnimalById(int userId);
+
+    Animal addAnimal (Animal animal);
+
+
+}
