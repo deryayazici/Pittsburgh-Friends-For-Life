e7994baca405cbc3e2e673004e25190d2df19757
Gino Faraci <faraci77@gmail.com>
2022-12-07 14:36:51 -0500

    display animals on homepage

--- vue/src/components/AnimalCard.vue
+++ vue/src/components/AnimalCard.vue
@@ -1,40 +1,75 @@
 <template>
-  <article class="picture">
-    <div class="pic-container">
-      <router-link to="/animals/${id}">
-        <img src="../../catPic.jpg" alt="" />
-      </router-link>
-    </div>
-  </article>
+  <section>
+    <article class="picture">
+      <div class="pic-container">
+        <!-- <router-link to="/animals/${id}"> -->
+        <img
+          v-for="pic in pics"
+          :key="pic"
+          :src="require(`@/assets/${pic}`)"
+          alt=""
+        />
+        <!-- </router-link> -->
+      </div>
+    </article>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      pics: ["../../catPic.jpg", "../../rabbit.jpg"],
+      pics: [
+        "catPic.jpg",
+        "rabbit.jpg",
+        "dog.jpg",
+        "ferret.jpg",
+        "indy.jpg",
+        "ferret2.jpg",
+      ],
     };
   },
 };
 </script>
 
 <style>
-img {
-  display: block;
-  width: 100%;
-}
 .home {
   background-color: #fffffe;
   min-height: 100vh;
 }
-.picture {
+img {
+  display: block;
+  width: 100%;
+  margin: 10px 0 20px;
+}
+.pic-container {
+  margin: 20px 0;
   background-color: #fffffe;
   padding: 20px 25px;
   border: 2px solid #fffffe;
   width: 90vw;
-  margin: 0 auto;
-  max-width: 400px;
+  margin: 1.5rem auto;
+  max-width: 300px;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.4);
   border-radius: 4px;
+}
+
+@media screen and (min-width: 500px) {
+  .pic-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    min-width: 90vw;
+    row-gap: 20px;
+    column-gap: 20px;
+  }
+  img {
+    object-fit: cover;
+    height: 300px;
+  }
+  @media screen and (min-width: 768px) {
+    .pic-container {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
 }
 </style>
