8bff991e59ac7d6e75124ef5109fd445872bc87b
Gino Faraci <faraci77@gmail.com>
2022-12-14 15:52:58 -0500

    styles

--- vue/src/components/UpdateAnimal.vue
+++ vue/src/components/UpdateAnimal.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <form class="update-pet" @submit.prevent="update">
+    <form class="update-pet" @submit.prevent="update">
       <h1 class="h3 mb-3 font-weight-normal">Update this animal</h1>
       <section class="sec-pet">
         <label for="type" class="pet">Type</label>
@@ -8,13 +8,13 @@
 
         <label for="breed" class="pet">Breed</label>
         <input type="text" v-model="updateAnimal.breed" />
-        
-        <label for="age" class="pet">Age</label >
+
+        <label for="age" class="pet">Age</label>
         <input type="number" v-model="updateAnimal.age" />
-        
+
         <label for="size" class="pet">Size</label>
         <input type="text" v-model="updateAnimal.size" />
-        
+
         <label for="temperament" class="pet">Temperament</label>
         <input type="text" v-model="updateAnimal.temperament" />
 
@@ -26,12 +26,12 @@
 
         <label for="photo">Add photo</label>
         <input type="text" v-model="updateAnimal.photo" />
-        
+
         <label for="is-adopted" class="pet">Adopted?</label>
         <input type="checkbox" v-model="updateAnimal.isAdopted" />
 
-        <button class="btn-pet" type="submit" >Submit</button>
-      </section>    
+        <button class="btn-pet" type="submit">Submit</button>
+      </section>
     </form>
   </div>
 </template>
@@ -39,38 +39,37 @@
 <script>
 import animalService from "@/services/AnimalService.js";
 export default {
+  props: ["animal"],
 
-    props: ["animal"],
+  data() {
+    return {
+      updateAnimal: {
+        animalId: this.$route.params.animalId,
+        name: this.animal.name,
+        type: this.animal.type,
+        breed: this.animal.breed,
+        age: this.animal.age,
+        size: this.animal.size,
+        temperament: this.animal.temperament,
+        specialNeeds: this.animal.specialNeeds,
+        photo: this.animal.photo,
+        isAdopted: this.animal.isAdopted,
+      },
+    };
+  },
 
-    data() {
-        return {
-            updateAnimal: {
-                animalId: this.$route.params.animalId,
-                name: this.animal.name,
-                type: this.animal.type,
-                breed: this.animal.breed,
-                age: this.animal.age,
-                size: this.animal.size,
-                temperament: this.animal.temperament,
-                specialNeeds: this.animal.specialNeeds,
-                photo: this.animal.photo,
-                isAdopted: this.animal.isAdopted,
-
-            }
-        }
+  methods: {
+    update() {
+      animalService
+        .updateAnimal(this.updateAnimal.animalId, this.updateAnimal)
+        .then((response) => {
+          if (response.status === 200) {
+            this.$router.push("/");
+          }
+        });
     },
-
-    methods: {
-        update() {
-            animalService.updateAnimal(this.updateAnimal.animalId, this.updateAnimal).then((response) => {
-                if (response.status === 200) {
-                    this.$router.push("/");
-                }
-            });
-        }
-    }
-
-}
+  },
+};
 </script>
 
 <style>
@@ -103,6 +102,9 @@ html {
   border: 10px solid #fffffe;
   border-radius: 8px;
   background-color: #fffffe;
+  width: 90vw;
+  max-width: 600px;
+  margin: 1rem auto;
 }
 
 .sec-pet {
@@ -126,5 +128,10 @@ html {
   color: #fffffe;
   font-weight: bold;
   cursor: pointer;
+  font-family: inherit;
+  letter-spacing: 4px;
+}
+.btn-pet:hover {
+  background-color: #ffc6c7;
 }
 </style>
--- vue/src/views/UserPage.vue
+++ vue/src/views/UserPage.vue
@@ -1,47 +1,69 @@
 <template>
-<div>
+  <div>
     <admin-panel v-if="checkAuthorizationLevel === 'ROLE_ADMIN'"></admin-panel>
-    <volunteer-panel v-if="checkAuthorizationLevel === 'ROLE_VOLUNTEER' || checkAuthorizationLevel === 'ROLE_ADMIN'"></volunteer-panel>
-    <button class="displayForm" v-if="checkAuthorizationLevel === 'ROLE_VOLUNTEER' || checkAuthorizationLevel === 'ROLE_ADMIN'" @click ="showForm">Add Pet</button>
-    <add-pet v-if="isFormExtended" ></add-pet>
-
-</div>
-  
+    <volunteer-panel
+      v-if="
+        checkAuthorizationLevel === 'ROLE_VOLUNTEER' ||
+        checkAuthorizationLevel === 'ROLE_ADMIN'
+      "
+    ></volunteer-panel>
+    <button
+      class="displayForm"
+      v-if="
+        checkAuthorizationLevel === 'ROLE_VOLUNTEER' ||
+        checkAuthorizationLevel === 'ROLE_ADMIN'
+      "
+      @click="showForm"
+    >
+      {{ isFormExtended ? "Hide Form" : "Add Pet" }}
+    </button>
+    <add-pet v-if="isFormExtended"></add-pet>
+  </div>
 </template>
 
 <script>
-import AdminPanel from '../components/AdminPanel.vue'
-import VolunteerPanel from '../components/VolunteerPanel.vue'
- import AddPet from '../components/AddPet.vue'
+import AdminPanel from "../components/AdminPanel.vue";
+import VolunteerPanel from "../components/VolunteerPanel.vue";
+import AddPet from "../components/AddPet.vue";
 
 export default {
-       data() {
-  return {
-    
-   
-    isFormExtended: false,
-  }
-       },
-    
+  data() {
+    return {
+      isFormExtended: false,
+    };
+  },
 
   components: { AdminPanel, VolunteerPanel, AddPet },
 
   computed: {
     checkAuthorizationLevel() {
-        return this.$store.state.user.authorities[0].name;
+      return this.$store.state.user.authorities[0].name;
     },
-},
+  },
 
-methods:{
-     showForm() {
+  methods: {
+    showForm() {
       this.isFormExtended = !this.isFormExtended;
     },
-}
-
-
-}
+  },
+};
 </script>
 
 <style>
-
+.displayForm {
+  display: block;
+  background-color: #ff8ba7;
+  border: 2px solid transparent;
+  width: 50%;
+  margin: 2rem auto;
+  color: #fffffe;
+  font-family: inherit;
+  padding: 0.35rem 0.75rem;
+  border-radius: 8px;
+  letter-spacing: 4px;
+  cursor: pointer;
+}
+.displayForm:hover {
+  background-color: #ffc6c7;
+}
 </style>
