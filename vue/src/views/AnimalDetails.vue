37bd3ed1868b8eafed5dd02fca62f80acd8a879f
Gino Faraci <faraci77@gmail.com>
2022-12-14 14:25:38 -0500

    random styles

--- vue/src/views/AnimalDetails.vue
+++ vue/src/views/AnimalDetails.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <animal-info :animal="animal">
-      
-    </animal-info>
-    <router-link :to="{name:'register'}" v-if="!checkToken" >Please register before adopting</router-link>
-    <button class="showForm" @click ="showForm" v-if="checkToken">Adopt</button>
+    <animal-info :animal="animal"> </animal-info>
+    <router-link :to="{ name: 'register' }" v-if="!checkToken"
+      >Please register before adopting</router-link
+    >
+    <div class="adopt-btn-div">
+      <button class="showForm" @click="showForm" v-if="checkToken">
+        {{ showQuestionnaire ? "Hide Form" : "Adopt" }}
+      </button>
+    </div>
+
     <adoption-questionnaire v-if="showQuestionnaire"></adoption-questionnaire>
- 
   </div>
 </template>
 
@@ -15,12 +19,10 @@ import AnimalInfo from "@/components/AnimalInfo.vue";
 import animalService from "@/services/AnimalService.js";
 import AdoptionQuestionnaire from "@/components/AdoptionQuestionnaire.vue";
 
-
 export default {
-   
   data() {
     return {
-      showQuestionnaire:false,
+      showQuestionnaire: false,
       animal: {},
     };
   },
@@ -34,10 +36,10 @@ export default {
         });
     },
     showForm() {
-      this.showQuestionnaire = !this.showQuestionnaire;   
-    }
+      this.showQuestionnaire = !this.showQuestionnaire;
+    },
   },
-   
+
   created() {
     this.displayAnimalsDetails();
   },
@@ -49,15 +51,31 @@ export default {
     checkToken() {
       return this.$store.state.user.username != null;
     },
-
- 
-},
-}
-
+  },
+};
 </script>
 
 <style>
 .animal-section-details {
   display: flex;
+  background-color: #fbfbbb;
+}
+.adopt-btn-div {
+  width: 90vw;
+  margin: 2rem auto;
+  text-align: center;
+}
+.showForm {
+  background-color: #ff8ba7;
+  padding: 0.25rem 0.75rem;
+  width: 50%;
+  border: 2px solid transparent;
+  border-radius: 8px;
+  font-family: inherit;
+  color: #fffffe;
+  cursor: pointer;
+}
+.showForm:hover {
+  background-color: #ffc6c7;
 }
 </style>
