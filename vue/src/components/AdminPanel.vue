3b88015bdd99eea07dea2ef84107b2d94bf654f8
Gino Faraci <faraci77@gmail.com>
2022-12-15 13:42:31 -0500

    polish

--- vue/src/components/AdminPanel.vue
+++ vue/src/components/AdminPanel.vue
@@ -6,11 +6,11 @@
       v-for="volunteer in pendingVolunteers"
       v-bind:key="volunteer.volunteerId"
     >
-      <div>
+      <div v-if="!isApproved" class="pending-card">
         <!-- <h2>{{volunteer.volunteerId}}</h2> -->
         <h2>{{ volunteer.firstName }} {{ volunteer.lastName }}</h2>
-        <h3>{{ volunteer.address }}</h3>
-        <h3>{{ volunteer.phoneNumber }}</h3>
+        <p>{{ volunteer.address }}</p>
+        <p>{{ volunteer.phoneNumber }}</p>
         <button
           class="btn-admin"
           type="submit"
@@ -38,6 +38,7 @@ export default {
   data() {
     return {
       pendingVolunteers: [],
+      isApproved: false,
     };
   },
   methods: {
@@ -52,6 +53,7 @@ export default {
         if (response.status === 201) {
           this.$router.push("/settings");
         }
+        this.isApproved = true;
       });
     },
     reject(id) {
@@ -77,4 +79,42 @@ export default {
   text-align: center;
   margin: 1.5rem auto;
 }
+.section-pending {
+  display: flex;
+  justify-content: center;
+}
+.pending-card {
+  background-color: #fffffe;
+  padding: 30px 40px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 8px;
+  margin: 1rem auto;
+}
+.pending-card h2 {
+  letter-spacing: 4px;
+  margin-bottom: 10px;
+  text-transform: capitalize;
+  text-align: center;
+}
+.pending-card p {
+  margin-bottom: 10px;
+}
+.pending-card .btn-admin {
+  display: inline-block;
+  margin: 10px 25px;
+  background-color: #ff8ba7;
+  color: #fffffe;
+  padding: 0.25rem 0.75rem;
+  font-family: inherit;
+  letter-spacing: 2px;
+  border: 2px solid transparent;
+  border-radius: 8px;
+  margin-bottom: 5px;
+}
+.pending-card .btn-admin:hover,
+.btn-admin:active {
+  background-color: #ffc6c7;
+}
 </style>
+
+// --clr-1: #ff8ba7; // --clr-2: #3cf157; // --clr-3: #fbfbbb; #ffc6c7
--- vue/src/components/VolunteerPanel.vue
+++ vue/src/components/VolunteerPanel.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h1 class="volunteer-title">Our Volunteers</h1>
-    <div
-      v-for="volunteer in approvedVolunteer"
-      v-bind:key="volunteer.volunteerId"
-      v-bind:volunteer="volunteer"
-    >
-      <h2>{{ volunteer.firstName }} {{ volunteer.lastName }}</h2>
-      <h3>{{ volunteer.address }}</h3>
-      <h3>{{ volunteer.phoneNumber }}</h3>
+    <div class="volunteer-card-container">
+      <div
+        class="volunteer-card"
+        v-for="volunteer in approvedVolunteer"
+        v-bind:key="volunteer.volunteerId"
+        v-bind:volunteer="volunteer"
+      >
+        <h2>{{ volunteer.firstName }} {{ volunteer.lastName }}</h2>
+        <p>{{ volunteer.address }}</p>
+        <p>{{ volunteer.phoneNumber }}</p>
+      </div>
     </div>
     <!-- <button class="add-pet-form" @click="showForm">Add Pet</button>
     <add-pet v-if="isFormExtended"></add-pet> -->
@@ -44,4 +47,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.volunteer-card-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.volunteer-card {
+  background-color: #fffffe;
+  padding: 30px 40px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 8px;
+  width: 90vw;
+  max-width: 25rem;
+  margin: 1rem auto;
+  background-color: #fffffe;
+}
+.volunteer-card h2 {
+  letter-spacing: 4px;
+  margin-bottom: 10px;
+  text-transform: capitalize;
+  text-align: center;
+}
+.volunteer-card p {
+  margin-bottom: 10px;
+}
+</style>
